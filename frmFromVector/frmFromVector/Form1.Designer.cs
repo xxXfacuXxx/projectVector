67ce5a3d48831dd4974c1dfcc229e257aba1ffe1
Facu Cutuli <103161141+xxXfacuXxx@users.noreply.github.com>
2023-08-12 10:56:42 -0300

    projectVector

---
+++ frmFromVector/frmFromVector/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace frmFromVector
+{
+    partial class frmFromVector
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnMax = new System.Windows.Forms.Button();
+            this.btnMin = new System.Windows.Forms.Button();
+            this.btnTotal = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.numVec = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(256, 16);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(225, 394);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(126, 117);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add Num";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnMax
+            // 
+            this.btnMax.Location = new System.Drawing.Point(511, 88);
+            this.btnMax.Name = "btnMax";
+            this.btnMax.Size = new System.Drawing.Size(75, 23);
+            this.btnMax.TabIndex = 2;
+            this.btnMax.Text = "Maximo";
+            this.btnMax.UseVisualStyleBackColor = true;
+            this.btnMax.Click += new System.EventHandler(this.btnMax_Click);
+            // 
+            // btnMin
+            // 
+            this.btnMin.Location = new System.Drawing.Point(511, 117);
+            this.btnMin.Name = "btnMin";
+            this.btnMin.Size = new System.Drawing.Size(75, 23);
+            this.btnMin.TabIndex = 3;
+            this.btnMin.Text = "Minimo";
+            this.btnMin.UseVisualStyleBackColor = true;
+            this.btnMin.Click += new System.EventHandler(this.btnMin_Click);
+            // 
+            // btnTotal
+            // 
+            this.btnTotal.Location = new System.Drawing.Point(511, 146);
+            this.btnTotal.Name = "btnTotal";
+            this.btnTotal.Size = new System.Drawing.Size(75, 23);
+            this.btnTotal.TabIndex = 4;
+            this.btnTotal.Text = "Total";
+            this.btnTotal.UseVisualStyleBackColor = true;
+            this.btnTotal.Click += new System.EventHandler(this.btnTotal_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(511, 175);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Limpiar";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // numVec
+            // 
+            this.numVec.Location = new System.Drawing.Point(126, 88);
+            this.numVec.Name = "numVec";
+            this.numVec.Size = new System.Drawing.Size(100, 20);
+            this.numVec.TabIndex = 6;
+            this.numVec.TextChanged += new System.EventHandler(this.numVec_TextChanged);
+            // 
+            // frmFromVector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numVec);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnTotal);
+            this.Controls.Add(this.btnMin);
+            this.Controls.Add(this.btnMax);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.listBox1);
+            this.Name = "frmFromVector";
+            this.Text = "Formulario de vectores";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnMax;
+        private System.Windows.Forms.Button btnMin;
+        private System.Windows.Forms.Button btnTotal;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox numVec;
+    }
+}
+
---
+++ frmFromVector/frmFromVector/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace frmFromVector
+{
+    public partial class frmFromVector : Form
+    {
+        int i = 0;
+        int[] vector = new int[25];
+
+        public frmFromVector()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (i < vector.Length)
+            {
+                if (int.TryParse(numVec.Text, out int num) && num > 0)
+                {
+                    vector[i] = num;
+                    i++;
+                    numVec.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("El numero debe ser mayor a 0");
+                }
+            }
+            else
+            {
+                MessageBox.Show("El Array está completo");
+            }
+            listBox1.Items.Clear();
+            for (int j = 0; j < i; j++)
+            {
+                listBox1.Items.Add(vector[j]);
+            }
+            int btnMax = vector[0];
+            for (int j = 1; j < i; j++)
+            {
+                if (vector[j] > btnMax)
+                {
+                    btnMax = vector[j];
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMax_Click(object sender, EventArgs e)
+        {
+            int maxValue = FindMaxValue();
+            if (maxValue != int.MinValue)
+            {
+                listBox1.Items.Add($"El valor máximo es: {maxValue}");
+            }
+            else
+            {
+                listBox1.Items.Add("No se han ingresado valores aún.");
+            }
+        }
+
+        private void btnMin_Click(object sender, EventArgs e)
+        {
+            int minValue = FindMinValue();
+            if (minValue != int.MaxValue)
+            {
+                listBox1.Items.Add($"El valor mínimo es: {minValue}");
+            }
+            else
+            {
+                listBox1.Items.Add("No se han ingresado valores aún.");
+            }
+        }
+        private int FindMaxValue()
+        {
+            if (i == 0)
+            {
+                return int.MinValue;
+            }
+
+            int maxValue = vector[0];
+            for (int j = 1; j < i; j++)
+            {
+                if (vector[j] > maxValue)
+                {
+                    maxValue = vector[j];
+                }
+            }
+
+            return maxValue;
+        }
+
+        private int FindMinValue()
+        {
+            if (i == 0)
+            {
+                return int.MaxValue;
+            }
+
+            int minValue = vector[0];
+            for (int j = 1; j < i; j++)
+            {
+                if (vector[j] < minValue)
+                {
+                    minValue = vector[j];
+                }
+            }
+
+            return minValue;
+        }
+        private void btnTotal_Click(object sender, EventArgs e)
+        {
+            int total = CalculateTotal();
+            listBox1.Items.Add($"La suma total es: {total}");
+        }
+        private int CalculateTotal()
+        {
+            int total = 0;
+            for (int j = 0; j < i; j++)
+            {
+                total += vector[j];
+            }
+            return total;
+        }
+
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void numVec_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
